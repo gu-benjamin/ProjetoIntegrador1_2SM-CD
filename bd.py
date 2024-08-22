52704c3abf3b2e5dec447a12cb57dbd7c70eb335
Gustavo Benjamin <gustasbenjamin@gmail.com>
2024-08-21 22:09:39 -0300

    Inicio projeto integrador

---
+++ bd.py
@@ -0,0 +1,47 @@
+# Simulaçao do nosso banco de dados
+
+Carros = [
+  
+  # Objeto 1
+  {
+    'id': 1,
+    'marca': 'Fiat',
+    'modelo': 'Marea',
+    'ano': 1990
+  },
+  # Objeto 2
+  {
+    'id': 2,
+    'marca': 'Ford',
+    'modelo': 'Escort',
+    'ano': 1985
+  },
+  # Objeto 3
+  {
+    'id': 3,
+    'marca': 'Renault',
+    'modelo': 'Kwid',
+    'ano': 2000
+  },
+  # Objeto 4
+  {
+    'id': 4,
+    'marca': 'Chevrolet',
+    'modelo': 'Chevette',
+    'ano': 1978
+  },
+  # Objeto 5
+  {
+    'id': 5,
+    'marca': 'Volkswagen',
+    'modelo': 'Fusca',
+    'ano': 1970
+  },
+  # Objeto 6
+  {
+    'id': 6,
+    'marca': 'Porshe',
+    'modelo': '911',
+    'ano': 2020
+  }
+]
